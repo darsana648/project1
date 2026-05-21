446c6ffb6487530a5727e54ea98e5848578a3a9b
N-meapp <developersnme@gmail.com>
2026-05-21 16:43:10 +0530

    first commit

--- project1/settings.py
+++ project1/settings.py
@@ -11,7 +11,7 @@ https://docs.djangoproject.com/en/6.0/ref/settings/
 """
 
 from pathlib import Path
-
+import os
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 
@@ -25,7 +25,7 @@ SECRET_KEY = 'django-insecure-@dck1an+$6!_22gi6lxct%ay5z%@6x(reagax(_)g=%_=&v&=3
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = [ 'darsana.pythonanywhere.com', '127.0.0.1', 'localhost' ]
 
 
 # Application definition
@@ -116,3 +116,4 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/6.0/howto/static-files/
 
 STATIC_URL = 'static/'
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
